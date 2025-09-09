77e9d09eb47b0f4f7459db984ea15145f1da86d8
cchen362 <ccl_1006@yahoo.com>
2025-09-09 13:34:07 +0800

    UI enhancement

--- frontend/src/components/ChatWindow/PromptLibraryButton.tsx
+++ frontend/src/components/ChatWindow/PromptLibraryButton.tsx
@@ -18,16 +18,7 @@ const PromptLibraryButton: React.FC<PromptLibraryButtonProps> = ({
       title="Browse prompt templates"
       type="button"
     >
-      <svg 
-        width="16" 
-        height="16" 
-        viewBox="0 0 24 24" 
-        fill="currentColor"
-        className="template-icon"
-      >
-        <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
-      </svg>
-      Templates
+📝 Templates
     </button>
   );
 };
